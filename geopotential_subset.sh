34219c62268e3830e95222462578582f68cefd99
Ewan Short <esh563@vdi-n2.novalocal>
2019-05-31 10:57:12 +1000

    spoke to claire

--- geopotential_subset.sh
+++ geopotential_subset.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-for i in $(seq -f "%02g" 05 14); do 
+for i in $(seq -f "%02g" 14 14); do 
     for j in $(seq -f "%02g" 01 31); do
         a=20$i
         b=$((a+1)) 
         season=$a$b 
         original=/g/data/ua8/ARCCSS_Data/MCASClimate/v1-0/$season/${1}/${2}_WRF_Maritime_Continent_4km_${a}12${j}.nc
         cropped=/g/data/w40/esh563/goulburn_NT/$season/${1}/${2}_goulburn_${a}12${j}.nc
-        echo $original 
-        ncks -d latitude,-13.0,-6.5 -d longitude,131.5,139.5 -d level,0 $original $cropped
+        ncks -d latitude,-13.0,-6.5 -d longitude,131.5,139.5 $original $cropped
     done
 done
