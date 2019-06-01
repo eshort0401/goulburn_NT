167fe3fa7ee24a0cff51b8ef82412964055a8711
Ewan Short <esh563@vdi-n14.novalocal>
2019-06-01 11:53:14 +1000

    some progress

---
+++ subset_again.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in $(seq -f "%02g" 14 14); do 
+   
+    a=20$i
+    b=$((a+1)) 
+    season=$a$b 
+    
+    for j in $(seq -f "%02g" 01 31); do
+        original=/g/data/w40/esh563/goulburn_NT/$season/${1}/${2}_goulburn_${a}12${j}.nc
+        cropped=/g/data/w40/esh563/goulburn_NT/$season/${1}/${2}_goulburn_${a}12${j}.nc
+        ncks -O -d latitude,-12.5,-8.0 -d longitude,133.0,136.0 $original $cropped
+    done
+done
